09ae96ca8ec4f5514f848260a2fb12fec8a678d2
runeb <runeberhardt@gmail.com>
2026-03-03 09:13:36 +0100

    graphs

--- experiment/sample.py
+++ experiment/sample.py
@@ -5,7 +5,22 @@ import experiment_runner as ExperimentRunner
 from experiment_configuration import ExperimentConfiguration
 from openfl.utils.async_writer import AsyncWriter
 
-config = ExperimentConfiguration(contribution_score_strategy="accuracy_only", minimum_rounds=10, force_merge_all=False, freerider_noise_scale=0, malicious_noise_scale=0, punish_factor=3, punish_factor_contrib=3, freerider_start_round=3, malicious_start_round=3) # OVERSKRIV variabler her for testing. eksempel: config = ExperimentConfiguration(minimum_rounds=1), hvis du kun vil køre een round#DATASET = "cifar-10"
+config = ExperimentConfiguration(
+    min_buy_in=int(1e18),
+    max_buy_in=int(1e18),
+    contribution_score_strategy="accuracy_loss",
+    use_outlier_detection=True,
+    minimum_rounds=10,
+    force_merge_all=False,
+    freerider_noise_scale=0.5,
+    malicious_noise_scale=0.5,
+    punish_factor=3,
+    punish_factor_contrib=3,
+    freerider_start_round=1,
+    malicious_start_round=1,
+)
+
+# OVERSKRIV variabler her for testing. eksempel: config = ExperimentConfiguration(minimum_rounds=1), hvis du kun vil køre een round#DATASET = "cifar-10"
 RESULTDATAFOLDER = Path(__file__).resolve().parent.joinpath("data/sample")
 DATASET = "mnist"
 
